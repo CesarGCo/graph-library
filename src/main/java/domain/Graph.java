0c583edc50c75c7f2f08c58fc7d60221f69c709a
César Grandis Costa <cesargrandis38@gmail.com>
2024-11-21 15:55:39 -0300

    feat: classe Graph implementada

--- src/main/java/domain/Graph.java
+++ src/main/java/domain/Graph.java
@@ -1,4 +1,27 @@
 package main.java.domain;
 
 public class Graph {
+    private Edge[][] adjacencyMatrix;
+
+    public Graph(int size) {
+        this.adjacencyMatrix = new Edge[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                this.adjacencyMatrix[i][j] = new Edge();
+            }
+        }
+    }
+
+    public void createEdge(int from, int to, int weight) {
+        this.adjacencyMatrix[from][to].setWeight(weight);
+        this.adjacencyMatrix[to][from].setEdge(true);
+    }
+
+    public void removeEdge(int from, int to) {
+        this.adjacencyMatrix[from][to].setEdge(false);
+    }
+
+    public Edge[][] getAdjacencyMatrix() {
+        return adjacencyMatrix;
+    }
 }
