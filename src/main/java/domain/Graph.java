229e1b21a27ba639d6efc706db2f636775291d5f
Ed-Games <edmarcosfilho2015@gmail.com>
2024-11-23 15:06:01 -0300

    feat: adiciona métodos de obter tamanho e ordem do grafo e altera createEdge e removeEdge para atuarem nos dois sentidos da aresta, já que o grafo é não direcionado

--- src/main/java/domain/Graph.java
+++ src/main/java/domain/Graph.java
@@ -5,23 +5,39 @@ public class Graph {
 
     public Graph(int size) {
         this.adjacencyMatrix = new Edge[size][size];
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 this.adjacencyMatrix[i][j] = new Edge();
             }
         }
     }
 
-    public void createEdge(int from, int to, int weight) {
+    public void createEdge(int from, int to, double weight) {
         this.adjacencyMatrix[from][to].setWeight(weight);
+        this.adjacencyMatrix[from][to].setEdge(true);
+        this.adjacencyMatrix[to][from].setWeight(weight);
         this.adjacencyMatrix[to][from].setEdge(true);
     }
 
     public void removeEdge(int from, int to) {
         this.adjacencyMatrix[from][to].setEdge(false);
+        this.adjacencyMatrix[to][from].setEdge(false);
     }
 
-    public Edge[][] getAdjacencyMatrix() {
-        return adjacencyMatrix;
+    public int getOrder() {
+        return adjacencyMatrix.length;
+    }
+
+    public int getSize() {
+        int count = 0;
+        int n = adjacencyMatrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (adjacencyMatrix[i][j].isEdge()) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
