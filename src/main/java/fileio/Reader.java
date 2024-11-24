74428f8ecb38764faa27d1004fb1cd9ab60f8071
Ed-Games <edmarcosfilho2015@gmail.com>
2024-11-24 14:40:40 -0300

    feat: atualiza reader para usar controller do grafo

--- src/main/java/fileio/Reader.java
+++ src/main/java/fileio/Reader.java
@@ -1,4 +1,5 @@
 package main.java.fileio;
+import main.java.controllers.GraphController;
 import main.java.domain.Graph;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,26 +7,26 @@ import java.io.IOException;
 
 public class Reader {
     private String filePath;
+    private GraphController graph;
 
-    public Reader (String filePath) {
+    public Reader (String filePath, GraphController graph) {
         this.filePath = filePath;
+        this.graph = graph;
     }
 
-    public Graph createGraph() {
+    public void createGraph() {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line = br.readLine();
             int size = (Integer.parseInt(line));
-            Graph graph = new Graph(size);
+            graph.createGraph(size);
 
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(" ");
-                graph.createEdge(Integer.parseInt(values[0])-1, Integer.parseInt(values[1])-1, Double.parseDouble(values[2]));
+                graph.addEdge(Integer.parseInt(values[0])-1, Integer.parseInt(values[1])-1, Double.parseDouble(values[2]));
             }
-            return graph;
 
         } catch (IOException e) {
             System.out.println("Erro ao abrir o arquivo: " + filePath);
-            return null;
         }
 
     }
