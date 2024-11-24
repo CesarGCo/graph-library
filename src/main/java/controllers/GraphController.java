40a93090183f6fb44b3462a746d1341ecbbb94f9
Ed-Games <edmarcosfilho2015@gmail.com>
2024-11-24 11:10:51 -0300

    feat: adiciona controller com os métodos do grafo

--- src/main/java/controllers/GraphController.java
+++ src/main/java/controllers/GraphController.java
@@ -1,4 +1,28 @@
 package main.java.controllers;
+import main.java.repositories.GraphRepository;
+import main.java.usecases.CreateGraphUSeCase;
+import main.java.usecases.GetGraphDensityUseCase;
+import main.java.usecases.addEdgeUseCase;
 
 public class GraphController {
+    public GraphRepository repository;
+
+    public GraphController() {
+        this.repository = new GraphRepository();
+    }
+
+    public void createGraph(int size) {
+        CreateGraphUSeCase uSeCase = new CreateGraphUSeCase(repository);
+        uSeCase.execute(size);
+    }
+
+    public void addEdge(int from, int to, double weight) {
+        addEdgeUseCase useCase = new addEdgeUseCase(repository);
+        useCase.execute(from, to, weight);
+    }
+
+    public Double getDensity() {
+        GetGraphDensityUseCase useCase = new GetGraphDensityUseCase(repository);
+        return useCase.execute();
+    }
 }
