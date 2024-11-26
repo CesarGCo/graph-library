e569ec591c3109344d762031bcee74c2bbc97ac8
Ed-Games <edmarcosfilho2015@gmail.com>
2024-11-25 23:26:08 -0300

    refactor: atualiza case para usar o graph repository

---
+++ src/main/java/usecases/GetVertexDegreeUseCase.java
@@ -0,0 +1,35 @@
+package main.java.usecases;
+
+import main.java.domain.Graph;
+import main.java.repositories.IGraphRepository;
+
+public class GetVertexDegreeUseCase {
+    private final IGraphRepository repository;
+
+    public GetVertexDegreeUseCase(IGraphRepository repository) {
+        this.repository = repository;
+    }
+
+    public Integer execute(int vertex) {
+        Graph graph = repository.getGraph();
+        if (graph == null) {
+            System.out.println("Graph is null");
+            return null;
+        }
+
+        vertex = vertex - 1;
+
+        if(vertex <0 || vertex > graph.getSize() ) {
+            System.out.println("Vertex does not exist");
+            return null;
+        }
+        int degree = 0;
+        int size = graph.getOrder();
+        for (int i = 0; i < size; i++) {
+            if (graph.getAdjacencyMatrix()[vertex][i].isEdge()) {
+                degree++;
+            }
+        }
+        return degree;
+    }
+}
